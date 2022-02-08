61c75a4335dd3fc0e6f98afcb720a782af9d9046
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2022-02-07 23:10:08 -0500

    Adding models for database

---
+++ Server/Data/AppDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.Data
+{
+    public class AppDBContext : DbContext
+    {
+        // Name for table on DB - References to Shared/Models/Category
+        public DbSet<Category> Categories { get; set; }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Model builder to use methods base.* & call Model(Category)
+            base.OnModelCreating(modelBuilder);
+            // Seed database
+            Category[] categoriesToSeed = new Category[3]; // Categories array
+            // Fill categories array with data.
+            for (int i = 0; i < 3; i++)
+            {
+                categoriesToSeed[i] = new Category
+                {
+                    CategoryID = i,
+                    BlogImage = "uploads/placeholder.jpg",
+                    Name = $"Category number: {i}",
+                    Description = $"Description for category number {i}",
+                };
+            }
+            // Call the category entity (Model) and fill with categories seeder(categoriesToSeed).
+            modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+        }
+    }
+}
---
+++ Shared/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Category
+    {
+        // Add table columns for database migration
+        [Key]
+        public int CategoryID { get; set; }
+        
+        [Required]
+        [MaxLength(256)]
+        public string BlogImage { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
+        public string Description { get; set; }
+    }
+}
